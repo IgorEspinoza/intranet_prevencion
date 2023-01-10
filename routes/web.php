f6ec064b7fcc9a64b94ea85e2898d14c7ce8e91d
igorEspinoza <igorespinoza10@gmail.com>
2023-01-10 17:57:30 -0300

    inicio proyecto / login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,10 +15,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/index', function(){
-    return view('index');
-});
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->middleware('auth');
+
+Route::get('/login', [SessionsController::class, 'create'])
+    ->middleware('guest') 
+    ->name('login.index') ;
+
+Route::post('/login', [SessionsController::class, 'store'])
+     ->name('login.store') ;
+
+Route::post('/logout', [SessionsController::class, 'destroy'])
+    ->middleware('auth') 
+    ->name('login.destroy') ;
